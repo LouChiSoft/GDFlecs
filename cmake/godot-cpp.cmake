116138c470341a6c749e581f52c093fde05ddfd3
Louis Child <louis@louischild.uk>
2024-12-09 18:59:28 +0000

    Fix CMake FetchContent

--- cmake/godot-cpp.cmake
+++ cmake/godot-cpp.cmake
@@ -1,19 +1,18 @@
-function(get_and_link_godot_cpp TARGET TAG PRECISION)
-    include(FetchContent)
+function(get_and_link_godot_cpp TARGET TAG PRECISION EXTENSION_FILE)
 
+    set(GODOT_CPP_SYSTEM_HEADERS ON CACHE INTERNAL "Generate system headers")
+    set(GODOT_CPP_WARNING_AS_ERROR OFF CACHE INTERNAL "Treat warnings as errors")
+    set(GENERATE_TEMPLATE_GET_NODE ON CACHE INTERNAL "Get template node")
+    set(FLOAT_PRECISION "${PRECISION}" CACHE INTERNAL "Library precision")
+    set(GODOT_CUSTOM_API_FILE "${EXTENSION_FILE}" CACHE INTERNAL "Extension api file")
+
+    include(FetchContent)
     FetchContent_Declare(
         godot-cpp
         GIT_REPOSITORY https://github.com/godotengine/godot-cpp.git
         GIT_TAG ${TAG}
         GIT_SUBMODULES_RECURSE 1
-        # Provide any configuration options you want, for instance:
-        CMAKE_ARGS
-          -DGODOT_CPP_SYSTEM_HEADERS=ON
-          -DGODOT_CPP_WARNING_AS_ERROR=OFF
-          -DGENERATE_TEMPLATE_GET_NODE=ON
-          -DFLOAT_PRECISION=${PRECISION}
     )
-
     FetchContent_MakeAvailable(godot-cpp)
 
     set_target_properties(godot-cpp PROPERTIES
@@ -22,7 +21,6 @@ function(get_and_link_godot_cpp TARGET TAG PRECISION)
     )
     # Turn on LTO and export compile commands for VSCode to use
     set(CMAKE_INTERPROCEDURAL_OPTIMIZATION_RELEASE ON)
-    set(CMAKE_EXPORT_COMPILE_COMMANDS ON) # WARNING: On Windows, this flag has no effect for Visual Studio generators. Ninja or Ninja Multi-Config is recommended
 
     target_link_libraries(${TARGET} PRIVATE godot-cpp)
 
